9ef95e2dc11b09557f3abf70404107ccc90bf529
kawsarm104 <kawsarm104@gmail.com>
2021-09-25 02:37:44 +0600

    added navbar

--- src/components/meal/Meal.jsx
+++ src/components/meal/Meal.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Meal = (props) => {
     console.log("showing all meals data line 4 in Meal.jsx component", props.meal)
-    const {strMeal,strCategory,strMealThumb,strInstructions} = props.meal
+    const {strMeal,strCategory,strMealThumb,strInstructions,strYoutube} = props.meal
     return (
         <div className='meal-cart col-12 col-md-6 col-lg-4 g-4
-        shadow rounded '>
+         rounded '>
 
     <div className="card-group">
   <div className="card">
@@ -14,7 +14,9 @@ const Meal = (props) => {
     <h3 className="card-title ">{strMeal}</h3>
     <h5>Category: {strCategory}</h5>
       <p className="card-text text-justify">{strInstructions.slice(0,300)}...</p>
-      <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+         <a href={strYoutube}
+                            className="text-decoration-none"><small className="text-primary">
+         Youtube Link:       {strYoutube}</small></a>
     </div>
   </div>
         </div>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,16 +2,29 @@ import React from 'react';
 import './Navbar.css';
 const Navbar = () => {
     return (
-        <div>
-            <nav>
-                <ul>
-  <li><a className="active" href="#home">Home</a></li>
-  <li><a href="#news">News</a></li>
-  <li><a href="#contact">Contact</a></li>
-  <li><a href="#about">About</a></li>
-</ul>
+        <div className="bg-warning p-4">
+         <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark p-2 ">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">MealDB</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+     
 
-            </nav>
+       
+      </ul>
+      <form className="d-flex">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button className="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
         </div>
     );
 };
